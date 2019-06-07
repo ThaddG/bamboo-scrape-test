241b6e2e2fd787c43d166624ce15d952759f64ca
ThaddG <45907136+ThaddG@users.noreply.github.com>
2019-06-07 15:59:45 -0400

    Add loop to create events using info from the lists

--- bambooScrape.py
+++ bambooScrape.py
@@ -9,44 +9,6 @@ import requests
 import calendar
 from bs4 import BeautifulSoup
 
-
-"""======================================
-  SCRAPED INFO
-======================================"""
-result = requests.get("https://www.eventbrite.com/o/bamboo-detroit-5830857275")
-src = result.content
-soup = BeautifulSoup(src, 'lxml')
-
-times = [] # hold the event times
-titles = [] # hold the titles of the events
-urls = [] # hold the event links
-
-
-liveTag = soup.find_all("article", {"id": "live_events"}) #store the live events div
-# step inside the live events article
-# in the form of a loop to get inside the article but will only loop once
-# since there is only one live_events article
-for tag in liveTag:
-    listCards = tag.find_all("div", {"class": "list-card-v2"}) #store the live cards
-    for card in listCards: #iterate through all cards and grab the info from each
-        ### TIMES ###
-        eventTimes = card.find("time", {"class": "list-card__date"})
-        times.append(eventTimes.text)
-        ### TITLE ###
-        eventTitle = card.find("div", {"class": "list-card__title"})
-        titles.append(eventTitle.text)
-        ### LINKS ###
-        a = card.find('a')
-        urls.append(a.attrs['href'])
-
-# uncomment to print the links
-# change "urls" to "times" or "titles" to loop through a different list 
-#for i in urls:
-#    print(i)
-"""======================================
-  SCRAPED INFO
-======================================"""
-
 scopes = ['https://www.googleapis.com/auth/calendar']
 flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
 
@@ -61,8 +23,6 @@ else:
       # dump the credentials obj that holds the credentials into a pickle file
       pickle.dump(credentials, open("token.pkl", "wb"))
       
-
-
 # load the credentials so it doesnt have to request permission again
 # this will most likely need an if statement
 #credentials = pickle.load(open("token.pkl", "rb"))
@@ -71,6 +31,9 @@ service = build("calendar", "v3", credentials=credentials)
 
 calendarResult = service.calendarList().list().execute()
 calendar_id = calendarResult['items'][0]['id']
+
+print(calendar_id)
+
 calendarResult = service.events().list(calendarId=calendar_id).execute()
 
 timezone = 'America/Detroit'
@@ -103,4 +66,48 @@ def create_event(start_time_str, summary, duration=1, description=None, location
       return service.events().insert(calendarId='primary', body=event).execute()
 
 
-create_event("7 june 6 PM", "Another Test Using Function")
+"""======================================
+  SCRAPED INFO
+======================================"""
+result = requests.get("https://www.eventbrite.com/o/bamboo-detroit-5830857275")
+src = result.content
+soup = BeautifulSoup(src, 'lxml')
+
+times = [] # hold the event times
+titles = [] # hold the titles of the events
+urls = [] # hold the event links
+
+
+liveTag = soup.find_all("article", {"id": "live_events"}) #store the live events div
+# step inside the live events article
+# in the form of a loop to get inside the article but will only loop once
+# since there is only one live_events article
+for tag in liveTag:
+    listCards = tag.find_all("div", {"class": "list-card-v2"}) #store the live cards
+    for card in listCards: #iterate through all cards and grab the info from each
+        ### TIMES ###
+        eventTimes = card.find("time", {"class": "list-card__date"})
+        times.append(eventTimes.text)
+        ### TITLE ###
+        eventTitle = card.find("div", {"class": "list-card__title"})
+        titles.append(eventTitle.text)
+        ### LINKS ###
+        a = card.find('a')
+        urls.append(a.attrs['href'])
+
+# create the events
+for i in range(len(titles)):
+      create_event(times[i], titles[i])
+
+# uncomment to print the links
+# change "urls" to "times" or "titles" to loop through a different list 
+#for i in urls:
+#    print(i)
+"""======================================
+  SCRAPED INFO
+======================================"""
+
+
+
+
+#create_event("7 june 6 PM", "Another Test Using Function")
